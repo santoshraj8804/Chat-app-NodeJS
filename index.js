6d5fb94c9ebe5d484d90c1e3d6e7bbcd57d0f8a2
Santosh raj <santoshrajgupta7366@gmail.com>
2025-06-24 21:29:14 +0530

    Added codes

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     console.log(' A User connected', socket.id);
 
-    socket.on('from_client', () => {
-        console.log('Event coming from client');
-    });
+    socket.on('msg_send', (data) => {
+        console.log(data);
+        io.emit('msg_recv', data);
+    })
 
-    setInterval(() => {
-        socket.emit('from_server');
-    }, 2000);
 });
 
 app.use('/', express.static(__dirname + '/public'));
